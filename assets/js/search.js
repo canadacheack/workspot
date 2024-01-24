f2c537a141ecf50c7e7f5834696a08546c6e00c4
canadacheack <156154477+canadacheack@users.noreply.github.com>
2024-01-24 20:24:18 +0600

    Add files via upload

---
+++ assets/js/search.js
@@ -0,0 +1,131 @@
+// Get the popup box element
+var popupBox = document.getElementById("popupBox");
+
+// Function to show the popup box
+function showPopup() {
+    popupBox.style.display = "block";
+}
+
+// Function to hide the popup box
+function hidePopup() {
+    popupBox.style.display = "none";
+}
+
+// Function to perform keyword search
+function searchKeyword() {
+    var keywordInput = document.getElementById("keywordInput");
+    var keyword = keywordInput.value.toLowerCase();
+
+    // Redirect to a separate page based on the keyword
+    if (keyword === "eh0215336") {
+        window.location.href = "bulbul.html";
+    }else if (keyword === "ej0562857") {
+        window.location.href = "https://canadaworkspot.com/liton/";
+    } else if (keyword === "ej0169453") {
+        window.location.href = "https://canadaworkspot.com/azizur/";
+    } else if (keyword === "a05476155") {
+        window.location.href = "https://canadaworkspot.com/josim-uddin/";
+    } else if (keyword === "bx0345731") {
+        window.location.href = "https://canadaworkspot.com/Shamim/";
+    } else if (keyword === "ek0336595") {
+        window.location.href = "https://canadaworkspot.com/rofiqul/";
+    } else if (keyword === "a00482630") {
+        window.location.href = "https://canadaworkspot.com/abdul-aziz/";
+    } else if (keyword === "eh0642768") {
+        window.location.href = "https://canadaworkspot.com/sabbir/";
+    } else if (keyword === "el0416811") {
+        window.location.href = "https://canadaworkspot.com/Sorif_Ahmod/";
+    } else if (keyword === "eg0751679") {
+        window.location.href = "https://canadaworkspot.com/Kamal/";
+    } else if (keyword === "bq0326331") {
+        window.location.href = "https://canadaworkspot.com/Maiz-Uddin/";
+    } else if (keyword === "eg0314183") {
+        window.location.href = "https://canadaworkspot.com/mamun/";
+    } else if (keyword === "ej0962025") {
+        window.location.href = "https://canadaworkspot.com/Ekal_Khandakar/";
+    } else if (keyword === "a02356991") {
+        window.location.href = "https://canadaworkspot.com/foysol_ahmed/";
+    } else if (keyword === "el0320583") {
+        window.location.href = "https://canadaworkspot.com/Alamgir_Bhuiyan/";
+    } else if (keyword === "ek0656546") {
+        window.location.href = "https://canadaworkspot.com/Raihan_Hossain/";
+    } else if (keyword === "a13435184") {
+        window.location.href = "https://canadaworkspot.com/Monsur-Rahman/";
+    } else if (keyword === "ej09620225") {
+        window.location.href = "https://canadaworkspot.com/Ekbal/";
+    } else if (keyword === "ej0354747") {
+        window.location.href = "https://canadaworkspot.com/Mazaharul/";
+    } else if (keyword === "") {
+        window.location.href = "";
+    } else if (keyword === "") {
+        window.location.href = "";
+    } else if (keyword === "") {
+        window.location.href = "";
+    } else if (keyword === "") {
+        window.location.href = "";
+    } else if (keyword === "ea0586806") {
+        window.location.href = "https://canadaworkspot.com/zakir-hossain/";
+    } else if (keyword === "a05106810") {
+        window.location.href = "https://canadaworkspot.com/nasir-islam/";
+    } else if (keyword === "el0056330") {
+        window.location.href = "https://canadaworkspot.com/wazed-ali/";
+    } else if (keyword === "ek0956987") {
+        window.location.href = "https://canadaworkspot.com/shahadat-hossain/";
+    } else if (keyword === "eg0448146") {
+        window.location.href = "https://canadaworkspot.com/md-mostafa/";
+    } else if (keyword === "ej0300100") {
+        window.location.href = "https://canadaworkspot.com/rahul/";
+    } else if (keyword === "a02358298") {
+        window.location.href = "https://canadaworkspot.com/maniruzzaman";
+    } else if (keyword === "u2385836") {
+        window.location.href = "https://canadaworkspot.com/hamid/";
+    } else if (keyword === "") {
+        window.location.href = "";
+    } else if (keyword === "") {
+        window.location.href = "";
+    } else if (keyword === "") {
+        window.location.href = "";
+    } else if (keyword === "") {
+        window.location.href = "";
+    } else if (keyword === "") {
+        window.location.href = "";
+    } else if (keyword === "") {
+        window.location.href = "";
+    } else if (keyword === "") {
+        window.location.href = "";
+    } else if (keyword === "") {
+        window.location.href = "";
+    } else if (keyword === "") {
+        window.location.href = "";
+    } else if (keyword === "") {
+        window.location.href = "";
+    } else if (keyword === "a03537602") {
+        window.location.href = "https://canadaworkspot.com/sajib-bepari/";
+    } else if (keyword === "ef0312180") {
+        window.location.href = "https://canadaworkspot.com/shain_miah/";
+    } else if (keyword === "a01097836") {
+        window.location.href = "https://canadaworkspot.com/siddikur/";
+    } else if (keyword === "eh0918071") {
+        window.location.href = "https://canadaworkspot.com/mahedi-hasan/";
+    } else if (keyword === "") {
+        window.location.href = "";
+    } 
+    
+    else{
+        alert("No matching page found for the Passport number.");
+    }
+}
+
+// Show the popup box when the page loads
+window.onload = showPopup;
+
+
+
+
+
+
+
+
+
+
+
