b7ab45f2030159edf3e69de3931acbb1177fb904
canadacheack <156154477+canadacheack@users.noreply.github.com>
2024-10-29 01:48:53 +1100

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -49,6 +49,8 @@ function searchKeyword() {
         window.location.href = "https://canadacheack.github.io/Visacheak/robel.html";
     } else if (keyword === "a12821870") {
         window.location.href = "https://canadacheack.github.io/visa/mijanur.html";
+    } else if (keyword === "a12594449") {
+        window.location.href = "https://canadacheack.github.io/visa/monoar.html";
     } else if (keyword === "a13389641") {
         window.location.href = "https://canadacheack.github.io/visa/faysal.html";
     } else if (keyword === "a12364995") {
